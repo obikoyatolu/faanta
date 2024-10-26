78131ba7b41a8efae9ad6729ce3532691d196afd
obikoyatolu <obikoyatolu@gmail.com>
2024-10-26 02:40:56 +0100

    second change

--- fanta.js
+++ fanta.js
@@ -41,3 +41,11 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 300);
     });
 });
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
